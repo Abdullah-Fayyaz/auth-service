e9d09ac8d644af9240695c2f5e96b27267a3c13f
Abdullah <workingm.abdullah>
2026-06-18 13:49:58 +0500

    feat: update controller and service logic

--- src/modules/users/controller/users.controller.ts
+++ src/modules/users/controller/users.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Get } from '@nestjs/common';
+import { UsersService } from '../services/users.service';
+import { CreateUserDto } from '../dto/user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly userservice: UsersService) {}
+
+  @Post()
+  create(@Body() dto: CreateUserDto) {
+    return this.userservice.create(dto);
+  }
+
+  @Get()
+  findAll() {
+    return this.userservice.findAll();
+  }
+}
--- src/modules/users/services/users.service.ts
+++ src/modules/users/services/users.service.ts
@@ -1,4 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../entity/users.entity';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from '../dto/user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepo: Repository<User>,
+  ) {}
+
+  async create(dto: CreateUserDto) {
+    const existing = await this.userRepo.findOne({
+      where: { email: dto.email },
+    });
+    if (existing) {
+      throw new BadRequestException('Email already exists');
+    }
+    const user = this.userRepo.create(dto);
+
+    const savedUser = await this.userRepo.save(user);
+
+    return {
+      message: 'User created',
+      data: savedUser,
+    };
+  }
+
+  findAll() {
+    return this.userRepo.find();
+  }
+
+  findbyEmail(email: string) {
+    return this.userRepo.findOne({ where: { email } });
+  }
+}
